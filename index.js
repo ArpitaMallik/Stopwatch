812dea721b9a4a4c53969928ca48d73c0df444a7
Arpita Mallik <86406957+ArpitaMallik@users.noreply.github.com>
2023-04-20 02:47:54 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const timeDisplay = document.querySelector("#timeDisplay");
+const start = document.querySelector("#start");
+const pause = document.querySelector("#pause");
+const reset = document.querySelector("#reset");
+
+let startTime = 0;
+let elapsedTime = 0;
+let currentTime = 0;
+let paused = true;
+let intervalID;
+let hrs = 0;
+let mins = 0;
+let secs = 0;
+
+start.addEventListener("click", ()=>{
+    if(paused){
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalID = setInterval(updateTime, 1000);
+    }
+});
+pause.addEventListener("click", ()=>{
+    if(!paused){
+        paused = true;
+        elapsedTime = Date.now() - startTime;
+        clearInterval(intervalID);
+    }
+});
+reset.addEventListener("click", ()=>{
+    paused = true;
+    startTime = 0;
+    elapsedTime = 0;
+    currentTime = 0;
+    hrs = 0;
+    mins = 0;
+    secs = 0;
+    timeDisplay.textContent = "00:00:00";
+});
+
+function updateTime(){
+    elapsedTime = Date.now() - startTime;
+
+    secs = Math.floor((elapsedTime/1000) % 60);
+    mins = Math.floor((elapsedTime/(1000*60)) % 60);
+    hrs = Math.floor((elapsedTime/(1000*60*60)) % 60);
+
+    secs = forzeroes(secs);
+    mins = forzeroes(mins);
+    hrs = forzeroes(hrs);
+
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+}
+function forzeroes(time){
+    time = time.toString();
+    return (time.length < 2) ? ("0" + time): time;
+}
